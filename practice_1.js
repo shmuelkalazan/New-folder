d92bf7736b8eacc8cf3f85d7407e005a29ce6cbf
Efrat-Code <efrathalle17@gmail.com>
2024-09-11 15:38:16 +0300

    cntinue

--- practice_1.js
+++ practice_1.js
@@ -11,6 +11,8 @@ const points = [40, 100, 1, 5,8,8,40,10, 25, 10];
 function uniq(list){
     return list.filter(a => list.indexOf(a) === list.lastIndexOf(a));
 }
+
+
 console.log(uniq(points));
 
 
